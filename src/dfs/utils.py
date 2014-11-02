8b3c1eed7696c90849c8058540fc7ad44e69fb83
cxhiano <cxhiano@gmail.com>
2014-11-01 23:36:08 -0400

    recursive xml to dict

--- src/dfs/utils.py
+++ src/dfs/utils.py
@@ -1,8 +1,18 @@
 import xml.etree.ElementTree as ET
 
+def _xml2dict(node, dict_):
+    """ Recursively transform a xml element tree rooted at node to a dict """
+    for child in node.getchildren():
+        children = child.getchildren()
+        if len(children) == 0:
+            dict_[child.tag] = child.text
+        else:
+            tmp = {}
+            _xml2dict(child, tmp)
+            dict_[child.tag] = tmp
+
 def load_config(filename):
     conf = {}
     tree = ET.parse(filename)
-    for node in tree.getroot():
-        conf[node.tag] = node.text
+    _xml2dict(tree.getroot(), conf)
     return conf
