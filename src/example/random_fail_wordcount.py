a9beb7329906c4939c17b107f41aec2740ba0e72
Chao Zhang <zhangchao6865@gmail.com>
2014-11-10 09:16:40 -0500

    split wordcount into two

--- src/example/random_fail_wordcount.py
+++ src/example/random_fail_wordcount.py
@@ -1,10 +1,20 @@
+import random
+
 def map(key, value, out):
     for word in value.split():
         if len(word) > 0:
             out.put(word, 1)
+    if random.randint(0, 5) == 0:
+        print 'random failed'
+        while True:
+            pass
 
 def reduce(key, values, out):
     cnt = 0
     for v in values:
         cnt += int(v)
     out.put(key, cnt)
+    if random.randint(0, 5) == 0:
+        print 'random failed'
+        while True:
+            pass
