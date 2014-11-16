4b83a9ba84cefcb72fefc8cd578f29de93071452
cxhiano <cxhiano@gmail.com>
2014-11-16 10:18:05 -0500

    get/put command

--- src/client.py
+++ src/client.py
@@ -1,7 +1,9 @@
+import sys
 import Pyro4
 from core.configurable import Configurable
 from utils.rmi import retrieve_object
 import utils.serialize as serialize
+from utils.conf_loader import load_config
 
 class Client(Configurable):
     def __init__(self, conf):
@@ -16,26 +18,45 @@ class Client(Configurable):
         '''Submit a job
 
         Example job_conf:
-            'mapper': <module name of mapper>,
-            'reducer': <module name of reducer>,
+            'mapper': mapper function,
+            'reducer': reducer function,
             'cnt_reducers': <integer>,
             'inputs': <list of input names>,
             'output_dir': <output directory name>
         '''
         self.jobrunner.submit_job(serialize.dumps(job_conf))
 
-    def upload(self, fname, local_file):
+    def put(self, fname, local_file):
         '''Upload a file to dfs'''
+        local_file = open(local_file, 'r')
         self.namenode.create_file(fname)
         self.namenode.write_file(fname, 0, local_file.read())
         self.namenode.close_file(fname)
 
-    def download(self, fname, local_file):
+    def get(self, fname, local_file):
         '''Download a file from dfs'''
+        local_file = open(local_file, 'w')
         bytes_read = 0
         while True:
             buf = self.namenode.read_file(fname, bytes_read, 1024)
-            if nbytes == 0:
+            if len(buf) == 0:
                 break
             local_file.write(buf)
             bytes_read += len(buf)
+
+def usage():
+    print 'python client.py get <dfs_file_name> <local_file_name>'
+    print 'python client.py put <dfs_file_name> <local_file_name>'
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        usage()
+        sys.exit(0)
+
+    client = Client(load_config('conf/client.xml'))
+    if sys.argv[1] == 'get':
+        client.get(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'put':
+        client.put(sys.argv[2], sys.argv[3])
+    else:
+        usage()
--- src/main.py
+++ src/main.py
@@ -6,27 +6,20 @@ from utils.conf_loader import load_config
 if __name__ == '__main__':
     client = Client(load_config(sys.argv[1]))
 
-    if len(sys.argv) > 2:
-        # with open('output_1.txt', 'w') as f:
-        #     client.download()
-    else:
-        client.upload('input_1.txt', open('client.py', 'r'))
-        client.upload('input_2.txt', open('client.py', 'r'))
+    jobconf1 = {
+        'mapper': example.wordcount.map,
+        'reducer': example.wordcount.reduce,
+        'cnt_reducers': 2,
+        'inputs': ['input_1.txt', 'input_2.txt'],
+        'output_dir': 'output_1'
+    }
 
-        jobconf1 = {
-            'mapper': example.wordcount.map,
-            'reducer': example.wordcount.reduce,
-            'cnt_reducers': 2,
-            'inputs': ['input_1.txt', 'input_2.txt'],
-            'output_dir': 'output_1'
-        }
-
-        jobconf2 = {
-            'mapper': example.slow_wordcount.map,
-            'reducer': example.slow_wordcount.reduce,
-            'cnt_reducers': 2,
-            'inputs': ['input_1.txt', 'input_2.txt'],
-            'output_dir': 'output_2'
-        }
-        client.submit(jobconf2)
-        client.submit(jobconf1)
+    jobconf2 = {
+        'mapper': example.slow_wordcount.map,
+        'reducer': example.slow_wordcount.reduce,
+        'cnt_reducers': 2,
+        'inputs': ['input_1.txt', 'input_2.txt'],
+        'output_dir': 'output_2'
+    }
+    client.submit(jobconf1)
+    client.submit(jobconf2)
