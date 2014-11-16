4ce15e40ae18413f313fd4898c013abbf335c8c1
Chao Zhang <zhangchao6865@gmail.com>
2014-11-16 00:35:00 -0500

    Update main

--- src/main.py
+++ src/main.py
@@ -1,28 +1,32 @@
 import sys
-import example.wordcount as wordcount
+import example
 from client import Client
 from utils.conf_loader import load_config
 
 if __name__ == '__main__':
     client = Client(load_config(sys.argv[1]))
 
-    client.upload('input_1.txt', open('client.py', 'r'))
-    client.upload('input_2.txt', open('client.py', 'r'))
+    if len(sys.argv) > 2:
+        # with open('output_1.txt', 'w') as f:
+        #     client.download()
+    else:
+        client.upload('input_1.txt', open('client.py', 'r'))
+        client.upload('input_2.txt', open('client.py', 'r'))
 
-    jobconf1 = {
-        'mapper': wordcount.map,
-        'reducer': wordcount.reduce,
-        'cnt_reducers': 2,
-        'inputs': ['input_1.txt', 'input_2.txt'],
-        'output_dir': 'output_1'
-    }
+        jobconf1 = {
+            'mapper': example.wordcount.map,
+            'reducer': example.wordcount.reduce,
+            'cnt_reducers': 2,
+            'inputs': ['input_1.txt', 'input_2.txt'],
+            'output_dir': 'output_1'
+        }
 
-    jobconf2 = {
-        'mapper': wordcount.map,
-        'reducer': wordcount.reduce,
-        'cnt_reducers': 1,
-        'inputs': ['input_1.txt', 'input_2.txt'],
-        'output_dir': 'output_2'
-    }
-    client.submit(jobconf2)
-    client.submit(jobconf1)
+        jobconf2 = {
+            'mapper': example.slow_wordcount.map,
+            'reducer': example.slow_wordcount.reduce,
+            'cnt_reducers': 2,
+            'inputs': ['input_1.txt', 'input_2.txt'],
+            'output_dir': 'output_2'
+        }
+        client.submit(jobconf2)
+        client.submit(jobconf1)
