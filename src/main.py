273d1d42ba010c9c9809fa643f5473e5248eb044
Chao Zhang <zhangchao6865@gmail.com>
2014-11-16 11:13:40 -0500

    Import

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import sys
-import example
+from example import wordcount, keycount
 from client import Client
 from utils.conf_loader import load_config
 
@@ -7,17 +7,17 @@ if __name__ == '__main__':
     client = Client(load_config(sys.argv[1]))
 
     jobconf1 = {
-        'mapper': example.wordcount.map,
-        'reducer': example.wordcount.reduce,
+        'mapper': wordcount.map,
+        'reducer': wordcount.reduce,
         'cnt_reducers': 2,
         'inputs': ['input_1.txt', 'input_2.txt'],
         'output_dir': 'output_1'
     }
 
     jobconf2 = {
-        'mapper': example.slow_wordcount.map,
-        'reducer': example.slow_wordcount.reduce,
-        'cnt_reducers': 2,
+        'mapper': keycount.map,
+        'reducer': keycount.reduce,
+        'cnt_reducers': 1,
         'inputs': ['input_1.txt', 'input_2.txt'],
         'output_dir': 'output_2'
     }
