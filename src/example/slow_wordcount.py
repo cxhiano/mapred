affe7e12e1fe7ba884580ec3b92f7124b96ada33
cxhiano <cxhiano@gmail.com>
2014-11-15 23:58:03 -0500

    change random fail wc to slow word count

---
+++ src/example/slow_wordcount.py
@@ -0,0 +1,17 @@
+def map(key, value, out):
+    import time
+
+    for word in value.split():
+        if len(word) > 0:
+            out.put(word, 1)
+
+    time.sleep(1)
+
+def reduce(key, values, out):
+    import time
+
+    cnt = 0
+    for v in values:
+        cnt += int(v)
+    out.put(key, cnt)
+    time.sleep(1)
